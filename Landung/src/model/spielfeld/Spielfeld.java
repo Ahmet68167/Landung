ffe06e922fef9513ae9a64532d92704dcadc2948
Ahmet <Ahmet@192.168.1.50>
2014-12-05 18:06:49 +0100

    noch nicht fertig

--- Landung/src/model/spielfeld/Spielfeld.java
+++ Landung/src/model/spielfeld/Spielfeld.java
@@ -1,5 +1,44 @@
 package model.spielfeld;
 
+import model.spielstein.Spielstein;
+
 public class Spielfeld {
+	
+	private int groesse;
+	private Spielstein[][] spielbrett;
+	
+	public Spielfeld(int groesse) {
+		this.groesse = groesse;
+		initialisiereSpielbrett();
+	}
+	
+	public Spielfeld() {
+		this(5);
+	}
+	
+	private void initialisiereSpielbrett() {
+		this.spielbrett = new Spielstein[this.groesse][this.groesse];
+		leeren();
+	}
+	
+	public void leeren() {
+		Spielstein leer = new Spielstein('_');
+		
+		for(int zeile = 0; zeile < spielbrett.length; zeile++) {
+			for(int spalte = 0; spalte < spielbrett[zeile].length; spalte++) {
+				
+				this.spielbrett[zeile][spalte] = leer;
+				
+			}		
+		}
+	}
+	
+	public int getGroesse() {
+		return this.groesse;
+	}
+	
+	public Spielstein[][] getSpielstein() {
+		return this.spielbrett;
+	}
 	
 }
