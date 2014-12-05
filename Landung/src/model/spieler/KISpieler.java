f1a44a0c696ae7c2469d835916db691f4f1e8593
Ahmet <Ahmet@192.168.1.50>
2014-12-05 19:06:09 +0100

    Spieler erstellt

---
+++ Landung/src/model/spieler/KISpieler.java
@@ -0,0 +1,14 @@
+package model.spieler;
+
+import java.util.List;
+
+public class KISpieler extends Spieler{
+
+	private int schwierigkeitsstufe;
+	
+	public KISpieler(String name, List list, int schwierigkeitsstufe) {
+		super(name, list);
+		this.schwierigkeitsstufe = schwierigkeitsstufe;
+	}
+
+}
