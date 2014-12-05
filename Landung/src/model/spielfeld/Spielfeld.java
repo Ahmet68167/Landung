c3929fa6d8d1e5375456ba348802f624c6bd239a
Ahmet <Ahmet@192.168.1.50>
2014-12-05 17:40:55 +0100

    test

---
+++ Landung/src/model/spielfeld/Spielfeld.java
@@ -0,0 +1,5 @@
+package model.spielfeld;
+
+public class Spielfeld {
+	
+}
---
+++ Landung/src/model/spielstein/Spielstein.java
@@ -0,0 +1,11 @@
+package model.spielstein;
+
+public class Spielstein {
+	
+	private char spielstein;
+	
+	public Spielstein(char spielstein) {
+		this.spielstein = spielstein;
+	}
+	
+}
