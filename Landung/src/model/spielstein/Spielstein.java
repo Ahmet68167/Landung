f64db7d641b6a8d49492f1aaea96337305646c21
Ahmet <Ahmet@192.168.1.50>
2014-12-05 17:49:57 +0100

    pos hinzugefügt

--- Landung/src/model/spielstein/Spielstein.java
+++ Landung/src/model/spielstein/Spielstein.java
@@ -3,9 +3,11 @@ package model.spielstein;
 public class Spielstein {
 	
 	private char spielstein;
+	private int[] pos;
 	
 	public Spielstein(char spielstein) {
 		this.spielstein = spielstein;
+		int[] pos = {0, 0};
 	}
 	
 	public char getSpielstein() {
@@ -14,6 +16,14 @@ public class Spielstein {
 	
 	public void setSpielstein(char spielstein) {
 		this.spielstein = spielstein;
+	}
+	
+	public int[] getPos() {
+		return this.pos;
+	}
+	
+	public void setPos(int[] pos) {
+		this.pos = pos;
 	}
 	
 }
