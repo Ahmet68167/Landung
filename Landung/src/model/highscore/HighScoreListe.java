f51e407b8b3de60a15670f0013bd1f08941c96fa
christian <christian-klostermann@web.de>
2014-12-13 10:13:20 +0100

    HighScroeListe toString überprüft ob ein Eintrag in der Highscoreliste vorhanden, wenn nicht gibt es eine Nachricht zurück "kein Eintrag"

--- Landung/src/model/highscore/HighScoreListe.java
+++ Landung/src/model/highscore/HighScoreListe.java
@@ -10,20 +10,20 @@ public class HighScoreListe {
 	private static final int ANZAHL = 10;
 	private static final String dateiName = "highscore";
 	private static final File datei = new File(dateiName);
-	
-	private List<HighScore> liste ;
+
+	private List<HighScore> liste;
 	private FileHandler fileHandler;
-	
+
 	public HighScoreListe() {
 		this.fileHandler = new FileHandler();
-		
-		if(datei.exists())
+
+		if (datei.exists())
 			this.liste = ladeListe();
-		else {			
+		else {
 			this.liste = new LinkedList<>();
 			schreibeInDatei();
 		}
-		
+
 	}
 
 	public void neuerHighScore(String name, int punkte) {
@@ -33,26 +33,30 @@ public class HighScoreListe {
 		this.liste.sort(new HighScoreComparator());
 		schreibeInDatei();
 	}
-	
+
 	public void schreibeInDatei() {
-		 fileHandler.save(dateiName, this.liste);
+		fileHandler.save(dateiName, this.liste);
 	}
-	
+
 	public List<HighScore> ladeListe() {
 		List<HighScore> liste = new LinkedList<>();
 		liste = fileHandler.load(dateiName, liste);
 		liste.sort(new HighScoreComparator());
 		return liste;
 	}
-	
+
 	public String toString() {
 		String txt = "";
-		
-		for(int i = 0; i < this.liste.size() && i < ANZAHL; i++) {
-			txt += "Platz " + (i+1) + " " + this.liste.get(i) + "\n";
+
+		if (this.liste.size() != 0) {
+
+			for (int i = 0; i < this.liste.size() && i < ANZAHL; i++) {
+				txt += "Platz " + (i + 1) + " " + this.liste.get(i) + "\n";
+			}
+		} else {
+			txt = "kein Eintrag";
 		}
-		
 		return txt;
 	}
-	
+
 }
